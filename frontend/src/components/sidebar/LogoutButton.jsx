62ed231363899e70155c3270729c4a21442361c4
Sharadindu Das <sharadindudas774@gmail.com>
2025-04-01 18:17:44 +0530

    feat: Basic frontend done

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,17 @@
+import { BiLogOut } from "react-icons/bi";
+
+const LogoutButton = () => {
+    const loading = false;
+
+    return (
+        <div className="mt-auto">
+            {!loading ? (
+                <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+            ) : (
+                <span className="loading loading-spinner"></span>
+            )}
+        </div>
+    );
+};
+
+export default LogoutButton;
